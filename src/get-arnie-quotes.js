085c281185c3d0fd89ea9d7261e1a008e2b1a7a9
Yashpal Puri <YashpalPuri08@gmail.com>
2023-11-01 09:49:01 +1100

    Implement getArnieQuotes function

--- src/get-arnie-quotes.js
+++ src/get-arnie-quotes.js
@@ -1,8 +1,20 @@
 const { httpGet } = require('./mock-http-interface');
 
 const getArnieQuotes = async (urls) => {
-  // TODO: Implement this function.
-  // return results;
+  const results = [];
+  const promises = [];
+  urls.forEach(url => {
+    promises.push(httpGet(url));
+  });
+  const responses = await Promise.all(promises);
+  responses.forEach(response => {
+    if (response.status === 200) {
+      results.push({ 'Arnie Quote': JSON.parse(response.body).message });
+    } else {
+      results.push({ 'FAILURE': JSON.parse(response.body).message });
+    }
+  });
+  return results;
 };
 
 module.exports = {
